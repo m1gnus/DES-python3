30b6fe089fb8f44826f822ab483f809197a6fd6a
m1gnus <m1gnus@protonmail.com>
2020-01-24 20:14:08 +0100

    Added operation modes

--- DES.py
+++ DES.py
@@ -12,13 +12,7 @@ from Exp_Perms import *
 from Err_codes import *
 from utils import *
 
-"""
-TODO:
-Triple-DES
-modes of operation -- CBC OFB CFB counter_mode CCM
-"""
-
-def create_blocks(text, padding):
+def create_blocks(text: str, padding: bool) -> list:
     if len(text)%64 != 0:
         text = text + '00001000'*(8 - (len(text) % 64)//8)
     if padding:
@@ -27,19 +21,77 @@ def create_blocks(text, padding):
     B = re.findall(pattern, text)
     return B
 
-def ECB_encrypt(Blocks, K):
+def ECB_encrypt(Blocks: list, K: list) -> str:
     result=''
     for i in Blocks:
         result += DES_encrypt(i, K)
     return result
 
-def ECB_decrypt(Blocks, K):
+def ECB_decrypt(Blocks: list, K: list) -> str:
     result=''
     for i in Blocks:
         result += DES_decrypt(i, K)
     return result
 
-def create_keys(key): # Create round keys by following the schedule
+def CBC_encrypt(Blocks: list, K: list, IV: str) -> str:
+    result=''
+    for i in Blocks:
+        tmp = ''
+        for j in range(64):
+            tmp += xor(IV[j], i[j])
+        IV = DES_encrypt(tmp, K)
+        result += IV
+    return result
+
+def CBC_decrypt(Blocks: list, K: list, IV: str) -> str:
+    result=''
+    for i in Blocks:
+        tmp = ''
+        plain = DES_decrypt(i, K)
+        for j in range(64):
+            tmp += xor(IV[j], plain[j])
+        IV = i
+        result += tmp
+    return result
+
+def OFB_encrypt_decrypt(Blocks: list, K: list, IV: str) -> str:
+    result=''
+    for i in Blocks:
+        tmp = ''
+        IV = DES_encrypt(IV, K)
+        print(len(IV), len(i))
+        for j in range(64):
+            tmp += xor(i[j], IV[j])
+        result += tmp
+    return result
+
+def CFB_encrypt_decrypt(Blocks: list, K: list, IV: str) -> str:
+    result=''
+    for i in Blocks:
+        tmp = ''
+        IV = DES_encrypt(IV, K)
+        print(len(IV), len(i))
+        for j in range(64):
+            tmp += xor(i[j], IV[j])
+        IV = tmp
+        result += tmp
+    return result
+
+def CTR_encrypt_decrypt(Blocks: list, K: list, IV: str) -> str:
+    result=''
+    print(IV)
+    for i in Blocks:
+        tmp = ''
+        print(IV)
+        IV = increment_counter(IV)
+        print(IV)
+        t = DES_encrypt(IV, K)
+        for j in range(64):
+            tmp += xor(i[j], t[j])
+        result += tmp
+    return result
+
+def create_keys(key: str) -> tuple: # Create round keys by following the schedule
     print("\nGenerating key schedule from ", bin_to_hex(key), " ...")
     K = []
     C = []
@@ -56,7 +108,7 @@ def create_keys(key): # Create round keys by following the schedule
         print("{}: K = {}[{}] C = {}[{}] D = {}[{}]".format(i,K[i],len(K[i]), C[i], len(C[i]), D[i], len(D[i])))
     return C, D, K
 
-def round_func(A, J):
+def round_func(A: str, J: str) -> str:
     X = perm(A, E)
     B = ''
     for i in range(len(J)):
@@ -68,7 +120,7 @@ def round_func(A, J):
     C = perm(C, P)
     return C
 
-def DES_encrypt(plaintext, K):
+def DES_encrypt(plaintext: str, K: list) -> str:
     L = []
     R = []
     print("\nCalculating L and R:")
@@ -96,7 +148,7 @@ def DES_encrypt(plaintext, K):
 
     return ciphertext
 
-def DES_decrypt(ciphertext, K):
+def DES_decrypt(ciphertext: str, K: list) -> str:
     L = []
     R = []
     print("\nCalculating L and R:")
@@ -124,16 +176,29 @@ def DES_decrypt(ciphertext, K):
 
     return plaintext
 
-def menu(plaintext, ciphertext, key, IV, triple, mode, C, D, K, pad, raw):
-    B = create_blocks(plaintext, pad) if plaintext != None else create_blocks(ciphertext, pad)
+def menu(plaintext: str, ciphertext: str, key: str, IV: str, mode: str, C: list, D: list, K: list, pad: bool, raw: bool) -> None:
+    B = create_blocks((plaintext if plaintext is not None else ciphertext), pad)
     if mode == 'ECB':
         result = (ECB_encrypt(B, K) if plaintext != None else ECB_decrypt(B, K))
+    elif mode == 'CBC':
+        result = (CBC_encrypt(B, K, IV) if plaintext != None else CBC_decrypt(B, K, IV))
+    elif mode == 'OFB':
+        result = OFB_encrypt_decrypt(B, K, IV)
+    elif mode == 'CFB':
+        result = CFB_encrypt_decrypt(B, K, IV)
+    elif mode == 'CTR':
+        result = CTR_encrypt_decrypt(B, K, IV)
     else:
         print("Unknown mode of operation")
 
-    print("\nResult: ", (bin_to_hex(result) if not raw else print(hex_to_str(bin_to_hex(result)))))
+    result = bin_to_hex(result)
+    if raw:
+        result = hex_to_str(result)
 
-def main():
+    print("\nResult:", result)
+    exit(0)
+
+def main() -> None:
     parser = argparse.ArgumentParser(description="A python3 implementation on DES cryptosystem")
 
     parser.add_argument('-k', '--key', dest='key', action='store', default=None, help="KEY (8 bytes)")
@@ -142,8 +207,8 @@ def main():
     parser.add_argument('-i', '--init-vector', dest='iv', action='store', default=None, help="initialization vector iv")
     parser.add_argument('-x', '--hex', dest='hex_input', action='store_const', const=True, default=False, help="read input as hex value")
     parser.add_argument('-m', '--mode', dest='mode', action='store', default='ECB', help="mode of operation") #Migliora help e controlla che sia valida
-    parser.add_argument('-t', '--triple', dest='triple', action='store_const', const=True, default=False, help="Use Triple DES")
     parser.add_argument('-P', '--padding', dest='pad', action='store_const', const=True,  default=False, help="add 8 bytes of padding at the end of the ciphertext... use it for compatibility with cyberchef when the blocks are correctly divided without padding")
+    parser.add_argument('-I', '--pad-iv', dest='piv', action='store_const', const=True,  default=False, help="pad iv if it's not long 64 bytes")
     parser.add_argument('-r', '--raw', dest='raw', action='store_const', const=True,  default=False, help="set raw outputs")
 
     args = parser.parse_args()
@@ -157,20 +222,25 @@ def main():
         usage()
         exit(ERR_MISSINGKEY)
     elif (len(args.key) != 8 and not args.hex_input) or (len(args.key) != 16 and args.hex_input):
-        print("[ERROR]: the key must be of length 8")
+        print("[ERROR]: the key must be of length 8 (bytes)")
         usage()
         exit(ERR_BADKEYLENGTH)
     elif args.mode != 'ECB' and args.iv == None:
         print("[ERROR]: the requested mode needs an IV in order to work properly")
         usage()
         exit(ERR_IVNEEDED)
+    elif args.iv != None and (((len(args.iv) < 8 and not args.piv) or len(args.iv) > 8) if not args.hex_input else ((len(args.iv) < 16 and not args.piv) or len(args.iv) > 16)):
+        print("[ERROR]: the iv must be of length 8 (bytes)")
+        exit(ERR_BADIVLENGTH)
     else:
         plaintext = (hex_to_bin(args.plaintext) if args.hex_input else string_to_bin(args.plaintext)) if args.plaintext != None else None
         ciphertext = (hex_to_bin(args.ciphertext) if args.hex_input else string_to_bin(args.ciphertext)) if args.ciphertext != None else None
         key = hex_to_bin(args.key) if args.hex_input else string_to_bin(args.key)
         IV = (hex_to_bin(args.iv) if args.hex_input else string_to_bin(args.iv)) if args.iv != None else None
+        if IV is not None and args.piv:
+            IV = pad_bytes(IV, 64)
         C, D, K = create_keys(key)
-        menu(plaintext, ciphertext, key, IV, args.triple, args.mode, C, D, K, args.pad, args.raw)
+        menu(plaintext, ciphertext, key, IV, args.mode, C, D, K, args.pad, args.raw)
 
 if __name__=="__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,28 +1,29 @@
-from SBox import *
+from SBox import SB
+from Err_codes import ERR_OVERFLOW
 import re
 
-def pad_bytes(A, n):
+def pad_bytes(A: str, n: int) -> str:
     return '0'*(n-len(A))+A
 
-def shift_bytes_left(A, n):
+def shift_bytes_left(A: str, n: int) -> str:
     return A[n:]+A[:n]
 
-def shift_bytes_right(A, n):
+def shift_bytes_right(A: str, n: int) -> str:
     return A[-n:]+A[:-n]
 
-def perm(A, P):
+def perm(A: str, P: list) -> str:
     return ''.join([ A[i] for i in P ])
 
 def usage():
     print("[Usage]: python3 DES.py -k [key] -m CBC -p [plaintext] -i [IV]")
 
-def xor(A,B):
+def xor(A: str, B: str) -> str:
     if A==B:
         return '0'
     else:
         return '1'
 
-def string_to_bin(s):
+def string_to_bin(s: str) -> str:
     tmp = ''
     for i in s:
         x = ord(i)
@@ -31,18 +32,18 @@ def string_to_bin(s):
         tmp += pad_bytes(bin(x)[2:], 8)
     return tmp
 
-def bin_to_hex(b):
+def bin_to_hex(b: str) -> str:
     L = re.findall("....", b)
     tmp = ''.join( [hex(int(i,2))[2:] for i in L ] )
     return tmp
 
-def hex_to_bin(h):
+def hex_to_bin(h: str) -> str:
     tmp = ''
     for i in h:
         tmp += pad_bytes(bin(int(i,16))[2:], 4)
     return tmp
 
-def hex_to_str(h):
+def hex_to_str(h: str) -> str:
     tmp = ''
     if len(h) % 2 == 1:
         h = '0' + h
@@ -51,7 +52,23 @@ def hex_to_str(h):
         tmp += chr(int(i,16))
     return tmp
 
-def SBOX_sub(s, l):
+def SBOX_sub(s: str, l: int) -> str:
     i = int(s[0] + s[-1],2)
     j = int(s[1:-1], 2)
     return pad_bytes(bin(SB[l][i][j])[2:], 4)
+
+def increment_counter(b: str) -> str:
+    b = list(b)
+    n = len(b)
+    overflow = True
+    for i in range(n):
+        if b[n-i-1] == '1':
+            b[n-i-1] = '0'
+        else:
+            b[n-i-1] = '1'
+            overflow = False
+            break
+    if overflow:
+        print("[ERROR]: overflow while incrementing counter")
+        exit(ERR_OVERFLOW)
+    return ''.join(b)
